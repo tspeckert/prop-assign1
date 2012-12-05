2ac6d9d2f21dd6b70a0417ccff0b4600dbc5caee
Thomas Speckert <tspeckert@gmail.com>
2012-12-05 13:52:38 +0100

    added constructor for DelegationObject

--- src/DelegationObject.java
+++ src/DelegationObject.java
@@ -3,4 +3,9 @@ import java.util.*;
 class DelegationObject {
 	public DelegationObject proto;
 	public HashMap <String, DelegationFunction> functions;
+	
+	public DelegationObject() {
+		proto = null;
+		functions = new HashMap();
+	}
 }
